ac47f4935f6929dc02c19b12485e1e1df51d57af
Muhammad Al Ghifari <muhammad.alghifari@ifg-life.id>
2023-10-07 04:01:38 +0700

    add new feature port-forward

--- kibana.py
+++ kibana.py
@@ -20,7 +20,7 @@ for list_config in lines:
     print([index_config],list_config)
     index_config+=1
 
-default_config = lines[3]
+default_config = lines[5]
 print("Config default saat ini: "+default_config)
 
 config = input("pilih config (tekan enter untuk default): ")
@@ -35,10 +35,12 @@ get_pods = os.environ.get("get_pods_command").replace("<>",kubeconfig)
 describe_pod = os.environ.get("describe_pods_command").replace("<>",kubeconfig)
 logs_pods = os.environ.get("logs_pods_command").replace("<>",kubeconfig)
 delete_pods = os.environ.get("delete_pods_command").replace("<>",kubeconfig)
+port_forward = os.environ.get("port_forward_command").replace("<>",kubeconfig)
 
 #code configuration
 prompt="input service name: "
 parameter=":param"
+port=":port"
 
 
 class Behaviour:
@@ -56,7 +58,7 @@ class Behaviour:
 os.system("cls")
 print("Welcome To Kibana Simplify Cli")
 print("Anda tidak perlu mengetikan command yang panjang lagi :)")
-bh = Behaviour('Get Pods','Describe Pod','Logs pod','Delete Pod')
+bh = Behaviour('Get Pods','Describe Pod','Logs pod','Delete Pod','Port Forward')
 key = input("Choose Menu (enter for exit): ")
 if(key == "1"):
     os.system("cls")
@@ -85,6 +87,13 @@ if(key == "4"):
     service_name = input(prompt)
     delete_pods_with_param = delete_pods.replace(parameter,service_name)
     os.system(delete_pods_with_param)
+if(key == "5"):
+    os.system("cls")
+    os.system(get_pods)
+    service_name = input(prompt)
+    input_port = input("Masukan Port, Dipisahkan dengan spasi jika lebih dari 1: ")
+    port_forward_with_param_and_port = port_forward.replace(parameter,service_name).replace(port,input_port)
+    os.system(port_forward_with_param_and_port)
 
 #cooming soon improvement
 # import keyboard
